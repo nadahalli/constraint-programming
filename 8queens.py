aebb184819f8d6c46ef0a7f9fbb5df0d5ddc46c8
Tejaswi Nadahalli <nadahalli@gmail.com>
2013-03-05 16:09:42 +0530

    Changed closure to functools.partial

--- 8queens.py
+++ 8queens.py
@@ -1,15 +1,13 @@
 from constraint import Problem, AllDifferentConstraint
 from itertools import product
+from functools import partial
 
 columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 rows = [1, 2, 3, 4, 5, 6, 7, 8]
 
 mapping = dict((x, y) for x, y in zip(columns, rows))
 
-def get_bishop_func(x, y):
-    def bishop_func(a, b):
-        return abs(mapping[x] - mapping[y]) != abs(a - b)
-    return bishop_func
+def bishop_func(a, b, x, y): return abs(mapping[x] - mapping[y]) != abs(a - b) or a == b
 
 problem = Problem()
 
@@ -17,8 +15,7 @@ problem.addVariables(columns, rows)
 problem.addConstraint(AllDifferentConstraint())
 
 for column1, column2 in product(columns, columns):
-    if column1 < column2:
-        problem.addConstraint(get_bishop_func(column1, column2), (column1, column2))
+    problem.addConstraint(partial(bishop_func, x = column1, y = column2), (column1, column2))
 
 for solution in problem.getSolutions():
     print sorted(solution.iteritems())
